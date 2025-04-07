3b7a1558340306a4fb9609aed3e4c63d04cc627b
MEGANE <m093020099@gmail.com>
2025-04-07 13:36:05 +0800

    20250407 Daily

---
+++ Leetcode/Medium/0416_Partition Equal Subset Sum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    public class _0416_Partition_Equal_Subset_Sum
+    {
+        //runtime 84%
+        //DP經典題
+        //Time complexity O(n*target)
+        //Space complexity O(target)
+        public bool CanPartition(int[] nums)
+        {
+            var sum = nums.Sum();
+            var target = sum / 2;
+
+            if ((sum & 1) != 0)
+                return false;
+
+            var length = nums.Length;
+            var dp = new bool[target + 1];
+            //空集合必定可以
+            dp[0] = true;
+
+            foreach (var num in nums)
+            {
+                for (int i = target; i >= num; i--)
+                {
+                    dp[i] = dp[i - num] || dp[i];
+                }
+            }
+
+            return dp[target];
+        }
+    }
+}
