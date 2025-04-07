7e6c703e066191e4c33f2c00c5c492b8dc05eb52
MEGANE <m093020099@gmail.com>
2025-04-08 00:23:30 +0800

    20250408

---
+++ Leetcode/Easy/1652_Defuse the Bomb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    public class _1652_Defuse_the_Bomb
+    {
+        //runtime 100%
+        //帶修改
+        public int[] Decrypt(int[] code, int k)
+        {
+            var result = new int[code.Length];
+
+            if (k == 0)
+                return result;
+            int start = 1, end = k, sum = 0;
+
+            if (k < 0)
+            {
+                k = -k;
+                start = code.Length - k;
+                end = code.Length - 1;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                sum += code[i];
+            }
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                result[i] = sum;
+                sum -= code[(start++) % code.Length];
+                sum += code[(++end) % code.Length];
+            }
+            return result;
+        }
+    }
+}
---
+++ Leetcode/Medium/3254_Find the Power of K-Size Subarrays I.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    public class _3254_Find_the_Power_of_K_Size_Subarrays_I
+    {
+        //runtime 100%
+        //Time complexity O(n)
+        //Space complexity O(n)
+        //slidingWindow
+        public int[] ResultsArray(int[] nums, int k)
+        {
+            var idx = 0;
+            var result = new int[nums.Length - k + 1];
+            var passIdx = 0;
+            var consecutiveAmount = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > 0 && nums[i - 1] + 1 == nums[i])
+                {
+                    consecutiveAmount++;
+                }
+
+                if (i - passIdx + 1 > k)
+                {
+                    if (nums[passIdx] + 1 == nums[passIdx + 1])
+                    {
+                        consecutiveAmount--;
+                    }
+                    passIdx++;
+                }
+
+                if (i - passIdx + 1 == k)
+                {
+                    result[idx] = consecutiveAmount == k ? nums[i] : -1;
+                    idx++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
