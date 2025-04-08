a7ee97f1c09b178a54bade20b86e34815104d540
MEGANE <m093020099@gmail.com>
2025-04-09 00:04:37 +0800

    20250409

---
+++ Leetcode/Medium/0916_Word Subsets.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    public class _0916_Word_Subsets
+    {
+        public IList<string> WordSubsets(string[] words1, string[] words2)
+        {
+            //runtime 42%
+            //time complexity O(n * m)
+            //space complexity O(n)
+            var map = new Dictionary<int, int>();
+            var result = new List<string>();
+
+            foreach (var word in words2)
+            {
+                var temp = new int[26];
+                foreach (var c in word)
+                {
+                    temp[c - 'a']++;
+                }
+                for (int i = 0; i < 26; i++)
+                {
+                    if (temp[i] > map.GetValueOrDefault(i))
+                    {
+                        map[i] = temp[i];
+                    }
+                }
+            }
+
+            foreach (var word in words1)
+            {
+                var temp = new int[26];
+                foreach (var c in word)
+                {
+                    temp[c - 'a']++;
+                }
+
+                var isValid = true;
+
+                for (int i = 0; i < 26; i++)
+                {
+                    if (temp[i] < map.GetValueOrDefault(i))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (isValid)
+                {
+                    result.Add(word);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Leetcode/Medium/2779_Maximum Beauty of an Array After Applying Operation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    public class _2779_Maximum_Beauty_of_an_Array_After_Applying_Operation
+    {
+        public int MaximumBeauty(int[] nums, int k)
+        {
+            //runtime 53.85%
+            //time complexity O(nlogn) sort
+            //space complexity O(1)
+            //slidingWindow
+            Array.Sort(nums);
+            var left = 0;
+            var right = 0;
+
+            for (right = 0; right < nums.Length; right++)
+            {
+                if (nums[right] - nums[left] > 2 * k)
+                    left++;
+            }
+
+            return right - left;
+        }
+    }
+}
