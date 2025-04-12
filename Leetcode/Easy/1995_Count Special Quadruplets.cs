ea36af4bc2942377ef182173aa22edf327c4d127
MEGANE <m093020099@gmail.com>
2025-04-12 15:59:31 +0800

    20250412 Daily

---
+++ Leetcode/Easy/1995_Count Special Quadruplets.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    public class _1995_Count_Special_Quadruplets
+    {
+        //runtime 91%
+        //time complexity O(n^2)
+        //space complexity O(n)
+        public int CountQuadruplets(int[] nums)
+        {
+            var tmp = new Dictionary<int, int>();
+            var bound = nums.Length - 1;
+            var result = 0;
+
+            tmp.Add(nums[0] + nums[1], 1);
+            
+            //C
+            for (var i = 2; i < bound; i++)
+            {
+                //D
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    var sum = nums[j] - nums[i];
+                    if (tmp.ContainsKey(sum))
+                        result += tmp[sum];
+                }
+
+                for (var j = 0; j < i; j++)
+                {
+                    var sum = nums[j] + nums[i]; //a+b
+                    if (!tmp.TryAdd(sum, 1))
+                        tmp[sum]++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Leetcode/Easy/2309_Greatest English Letter in Upper and Lower Case.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    public class _2309_Greatest_English_Letter_in_Upper_and_Lower_Case
+    {
+        public string GreatestLetter(string s)
+        {
+            //runtime 18%
+            var letters = new int[128];
+            foreach (var c in s)
+            {
+                letters[c]++;
+            }
+
+            for (var i = 'z'; i >= 'a'; i--)
+            {
+                if (letters[i] > 0 && letters[i - 'a' + 'A'] > 0)
+                {
+                    return ((char)(i - 'a' + 'A')).ToString();
+                }
+            }
+
+            return "";
+
+            //runtime 40%
+            //可以再合併
+            //time complexity O(n)
+            //space complexity O(n)
+            var set = new HashSet<int>();
+            var contaiins = new HashSet<int>();
+            var gap = 'a' - 'A';
+            var result = 0;
+
+            foreach (var c in s)
+            {
+                //upper
+                if (c < 'a')
+                {
+                    if (set.Contains(c + gap))
+                    {
+                        if (!contaiins.Contains(c))
+                        {
+                            result = Math.Max(result, c);
+                            contaiins.Add(c);
+                        }
+                    }
+                }
+                else
+                {
+                    if (set.Contains(c - gap))
+                    {
+                        if (!set.Contains(c))
+                        {
+                            result = Math.Max(result, c - gap);
+                            contaiins.Add(c - gap);
+                        }
+                    }
+                }
+
+                set.Add(c);
+            }
+
+            return result != 0 ? ((char)result).ToString() : "";
+        }
+    }
+}
