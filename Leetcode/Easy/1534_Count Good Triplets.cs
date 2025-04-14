23eb88ee03fb1cea94e4394ffaf12bee7390c19b
MEGANE <m093020099@gmail.com>
2025-04-14 23:24:26 +0800

    20250414 Daily

---
+++ Leetcode/Easy/1534_Count Good Triplets.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    internal class _1534_Count_Good_Triplets
+    {
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            //runtime 72.3%
+            //time complexity: O(n^3)
+            //space complexity: O(1)
+            var resut = 0;
+
+            for (var i = 0; i < arr.Length - 2; i++)
+            {
+                for (var j = i + 1; j < arr.Length - 1; j++)
+                {
+                    for (var k = j + 1; k < arr.Length; k++)
+                    {
+                        if (Math.Abs(arr[i] - arr[j]) <= a && Math.Abs(arr[j] - arr[k]) <= b && Math.Abs(arr[i] - arr[k]) <= c)
+                            resut++;
+                    }
+                }
+            }
+
+            return resut;
+        }
+    }
+}
