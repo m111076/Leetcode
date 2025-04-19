4c777472071fa86f7ba42919903bc61f288f8702
MEGANE <m093020099@gmail.com>
2025-04-20 00:41:12 +0800

    20250420 Daily

---
+++ Leetcode/Medium/0038_Count and Say.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    internal class _0038_Count_and_Say
+    {
+        //public string CountAndSay(int n)
+        //{
+        //    if (n == 1) return "1";
+
+        //    var result = "1";
+
+        //    for (var i = 1; i < n; i++)
+        //    {
+        //        var sb = new StringBuilder();
+        //        var count = 1;
+        //        for (var j = 1; j < result.Length; j++)
+        //        {
+        //            if (result[j] == result[j - 1])
+        //            {
+        //                count++;
+        //            }
+        //            else
+        //            {
+        //                sb.Append(count);
+        //                sb.Append(result[j - 1]);
+        //                count = 1;
+        //            }
+        //        }
+        //        sb.Append(count);
+        //        sb.Append(result[result.Length - 1]);
+        //        result = sb.ToString();
+        //    }
+        //}
+    }
+}
---
+++ Leetcode/Medium/2563_Count the Number of Fair Pairs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    internal class _2563_Count_the_Number_of_Fair_Pairs
+    {
+        //runtime 100%
+        //time complexity O(nlogn) sort為n*logn
+        public long CountFairPairs(int[] nums, int lower, int upper)
+        {
+            Array.Sort(nums);
+            return FairPairsCounter(nums, upper) - FairPairsCounter(nums, lower - 1);
+        }
+
+        private long FairPairsCounter(int[] iNums, int iVal)
+        {
+            var result = 0L;
+
+            var right = iNums.Length - 1;
+
+            for (var left = 0; left < right; left++)
+            {
+                while (right > left && iNums[left] + iNums[right] > iVal)
+                {
+                    right--;
+                }
+
+                result += right - left;
+            }
+
+            return result;
+        }
+    }
+}
