a940a95373f73af5c1ffbd07b33537ad277660f7
MEGANE <m093020099@gmail.com>
2025-04-21 00:17:11 +0800

    20250420

---
+++ Leetcode/Easy/3232_Find if Digit Game Can Be Won.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    //runtime 100%
+    //time complexity O(n)
+    //space complexity O(1)
+    internal class _3232_Find_if_Digit_Game_Can_Be_Won
+    {
+        public bool CanAliceWin(int[] nums)
+        {
+            var tmp = 0;
+
+            foreach (int num in nums)
+            {
+                if (num.ToString().Length == 1)
+                {
+                    tmp += num;
+                }
+                else
+                {
+                    tmp -= num;
+                }
+            }
+
+            return tmp != 0;
+        }
+    }
+}
