b2c7801d530531ccc598e07889e4ea24a4cc2250
MEGANE <m093020099@gmail.com>
2025-04-22 13:01:41 +0800

    20250422

---
+++ Leetcode/Hard/2338_Count the Number of Ideal Arrays.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Hard
+{
+    internal class _2338_Count_the_Number_of_Ideal_Arrays
+    {
+
+    }
+}
---
+++ Leetcode/Medium/1400_Construct K Palindrome Strings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    //runtime 74%
+    //time complexity O(n)
+    //space complexity O(1) 
+    internal class _1400_Construct_K_Palindrome_Strings
+    {
+        public bool CanConstruct(string s, int k)
+        {
+            var cArr = new int[26];
+            var target = k;
+
+            foreach (var c in s)
+            {
+                cArr[c - 'a']++;
+            }
+
+            for (var i = 0; i < cArr.Length; i++)
+            {
+                if (cArr[i] % 2 == 1)
+                {
+                    k--;
+                }
+            }
+
+            return target <= s.Length && k >= 0;
+        }
+    }
+}
