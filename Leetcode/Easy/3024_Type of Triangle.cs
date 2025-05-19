c12f29a4e7a590cd4617eb6dfbabf426da4d7037
MEGANE <m093020099@gmail.com>
2025-05-20 00:15:30 +0800

    20250519

---
+++ Leetcode/Easy/3024_Type of Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    internal class _3024_Type_of_Triangle
+    {
+        public string TriangleType(int[] nums)
+        {
+            //runtime  12%
+            //time complexity O(1)
+            Array.Sort(nums);
+
+            if (nums[0] + nums[1] <= nums[2])
+                return "none";
+
+            var set = new HashSet<int>();
+            set.Add(nums[0]);
+            set.Add(nums[1]);
+            set.Add(nums[2]);
+
+            if (set.Count == 1)
+                return "equilateral";
+            else if (set.Count == 2)
+                return "isosceles";
+            else
+                return "scalene";
+        }
+    }
+}
