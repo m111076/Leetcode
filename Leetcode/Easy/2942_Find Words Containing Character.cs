2374a07848624eb62d0f27958a5e196fc73ed0d5
MEGANE <m093020099@gmail.com>
2025-05-24 23:24:53 +0800

    20250524

---
+++ Leetcode/Easy/2942_Find Words Containing Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    internal class _2942_Find_Words_Containing_Character
+    {
+        //runtime 37%
+        //time complexity O(w*n)
+        //space complexity O(w)
+        public IList<int> FindWordsContaining(string[] words, char x)
+        {
+            var result = new List<int>();
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                foreach (var c in words[i])
+                {
+                    if (c == x)
+                    {
+                        result.Add(i);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
