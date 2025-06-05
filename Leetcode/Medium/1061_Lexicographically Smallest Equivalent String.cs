a0a56bc329bd90b39332a94ada31154807412fc8
MEGANE <m093020099@gmail.com>
2025-06-05 23:44:32 +0800

    20250605

---
+++ Leetcode/Medium/1061_Lexicographically Smallest Equivalent String.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    //runtime 100%
+    //time complexity O(n)
+    internal class _1061_Lexicographically_Smallest_Equivalent_String
+    {
+        private int[] Chars = new int[26];
+
+        public string SmallestEquivalentString(string s1, string s2, string baseStr)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                Chars[i] = i;
+            }
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                Union(s1[i] - 'a', s2[i] - 'a');
+            }
+
+            var result = new StringBuilder();
+            foreach (char ch in baseStr)
+            {
+                var rep = Find(ch - 'a');
+                result.Append((char)(rep + 'a'));
+            }
+
+            return result.ToString();
+        }
+
+        private int Find(int x)
+        {
+            if (Chars[x] != x)
+            {
+                Chars[x] = Find(Chars[x]);
+            }
+            return Chars[x];
+        }
+
+        private void Union(int a, int b)
+        {
+            var rootA = Find(a);
+            var rootB = Find(b);
+            if (rootA < rootB)
+            {
+                Chars[rootB] = rootA;
+            }
+            else if (rootA > rootB)
+            {
+                Chars[rootA] = rootB;
+            }
+        }
+    }
+}
