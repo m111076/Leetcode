2ae7e4bb3f1b8492bf3fed677e5d7264232a1f5a
MEGANE <m093020099@gmail.com>
2025-06-26 00:40:18 +0800

    20250625

---
+++ Leetcode/Medium/0142_Linked List Cycle II.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    internal class _0142_Linked_List_Cycle_II
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            //runtime 84%
+            //time complexity O(n)
+            //space complexity O(1)
+            if (head == null || head.next == null)
+                return null;
+
+            var slow = head;
+            var fast = head;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                    break;
+            }
+
+            if (slow != fast)
+                return null;
+
+            slow = head;
+
+            while (slow != fast)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return slow;
+        }
+    }
+}
---
+++ Leetcode/Medium/0148_Sort List.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    internal class _0148_Sort_List
+    {
+        //runtime 77%
+        //time complexity O(nlogn)
+        //space complexity O(1)
+        public ListNode SortList(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            var mid = CheckMid(head);
+            var right = mid.next;
+            mid.next = null;
+
+            var lSort = SortList(head);
+            var rSort = SortList(right);
+
+            return Merge(lSort, rSort);
+        }
+
+        private ListNode CheckMid(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+            ListNode pre = null;
+
+            while (fast != null && fast.next != null)
+            {
+                pre = slow;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return pre;
+        }
+
+        private ListNode Merge(ListNode head1, ListNode head2)
+        {
+            var result = new ListNode();
+            var tmp = result;
+
+            while (head1 != null && head2 != null)
+            {
+                if (head1.val >= head2.val)
+                {
+                    tmp.next = head2;
+                    head2 = head2.next;
+                }
+                else
+                {
+                    tmp.next = head1;
+                    head1 = head1.next;
+                }
+
+                tmp = tmp.next;
+            }
+
+            tmp.next = head1 ?? head2;
+
+            return result.next;
+        }
+    }
+}
