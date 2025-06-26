52b9c248faded0517fccfdb85ee892aa6a8fe057
MEGANE <m093020099@gmail.com>
2025-06-27 00:22:33 +0800

    20250626

---
+++ Leetcode/Medium/0146_LRU Cache.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    internal class _0146_LRU_Cache
+    {
+        public class LRUCache
+        {
+            //runtime 76%
+            private class Node
+            {
+                public Node(int k, int v) { key = k; value = v; }
+
+                public int key, value;
+                public Node prev, next;
+            }
+
+            private int capacity;
+            private Dictionary<int, Node> cache = new Dictionary<int, Node>();
+            private Node dummy = new Node(0, 0);
+
+            public LRUCache(int capacity)
+            {
+                this.capacity = capacity;
+                dummy.prev = dummy;
+                dummy.next = dummy;
+            }
+
+            public int Get(int key)
+            {
+                var node = GetNode(key);
+
+                return node == null ? -1 : node.value;
+            }
+
+            public void Put(int key, int value)
+            {
+                var node = GetNode(key);
+
+                if (node != null)
+                    node.value = value;
+                else
+                {
+                    node = new Node(key, value);
+                    cache.Add(key, node);
+                    MoveToEnd(node);
+                    if (cache.Count > capacity)
+                    {
+                        var tmpEnd = dummy.prev;
+                        Remove(tmpEnd);
+                        cache.Remove(tmpEnd.key);
+                    }
+                }
+            }
+
+            private void Remove(Node node)
+            {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+            }
+
+            private Node GetNode(int key)
+            {
+                if (!cache.ContainsKey(key))
+                    return null;
+
+                var node = cache[key];
+                Remove(node);
+                MoveToEnd(node);
+
+                return node;
+            }
+
+            private void MoveToEnd(Node node)
+            {
+                node.prev = dummy;
+                node.next = dummy.next;
+                node.prev.next = node;
+                node.next.prev = node;
+            }
+        }
+    }
+}
---
+++ Leetcode/Medium/2311_Longest Binary Subsequence Less Than or Equal to K.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    internal class _2311_Longest_Binary_Subsequence_Less_Than_or_Equal_to_K
+    {
+        public int LongestSubsequence(string s, int k)
+        {
+            //runtime 100%
+            //time complexity O(n)
+            //space complexity O(1)
+            var sL = s.Length;
+            var prefixZeroCnt = 0;
+
+            for (var i = 31; i >= 0; i--)
+            {
+                if (((k >> i) & 1) == 0)
+                    prefixZeroCnt++;
+                else
+                    break;
+            }
+
+            var kL = 32 - prefixZeroCnt;
+
+            if (kL > sL)
+                return sL;
+
+            var tmp = Convert.ToInt32(s.Substring(sL - kL), 2);
+            var result = tmp <= k ? kL : kL - 1;
+
+            for (var i = 0; i < sL - kL; i++)
+            {
+                if (s[i] == '0')
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
