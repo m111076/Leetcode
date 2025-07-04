0abf144244ca1c9a0641cf396e97d1728e5fc3b3
MEGANE <m093020099@gmail.com>
2025-07-05 02:35:45 +0800

    20250704

---
+++ Leetcode/Easy/1394_Find Lucky Integer in an Array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    internal class _1394_Find_Lucky_Integer_in_an_Array
+    {
+        public int FindLucky(int[] arr)
+        {
+            var cnt = new int[501];
+
+            foreach (var item in arr)
+            {
+                cnt[item]++;
+            }
+
+            var result = -1;
+
+            for (var i = 500; i >= 1; i--)
+            {
+                if (i == cnt[i])
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Leetcode/Medium/0994_Rotting Oranges.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    internal class _0994_Rotting_Oranges
+    {
+        private List<int[]> direction = new List<int[]>
+        {
+            new int[]{1,0 },
+            new int[]{0,1},
+            new int[]{-1,0},
+            new int[]{0,-1},
+        };
+        public int OrangesRotting(int[][] grid)
+        {
+            var rottenQueue = new Queue<int[]>();
+            var fresh = 0;
+
+            for (var i = 0; i < grid.Length; i++)
+            {
+                for (var j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                        fresh++;
+                    else if (grid[i][j] == 2)
+                        rottenQueue.Enqueue(new int[] { i, j });
+                }
+            }
+
+            var result = 0;
+
+            while (fresh > 0 && rottenQueue.Count > 0)
+            {
+                result++;
+
+                var size = rottenQueue.Count;
+
+                for (var i = 0; i < size; i++)
+                {
+                    var tmp = rottenQueue.Dequeue();
+
+                    foreach (var d in direction)
+                    {
+                        var tmpi = tmp[0] + d[0];
+                        var tmpj = tmp[1] + d[1];
+
+                        if (tmpi >= 0 && tmpi < grid.Length && tmpj >= 0 && tmpj < grid[0].Length && grid[tmpi][tmpj] == 1)
+                        {
+                            rottenQueue.Enqueue(new int[] { tmpi, tmpj });
+                            grid[tmpi][tmpj] = 2;
+                            fresh--;
+                        }
+                    }
+                }
+            }
+
+            return fresh > 0 ? -1 : result;
+        }
+    }
+}
