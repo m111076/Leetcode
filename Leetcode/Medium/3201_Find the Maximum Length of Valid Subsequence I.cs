e2a83c00358ef45d81846d37849e11542bbf87cb
MEGANE <m093020099@gmail.com>
2025-07-16 01:51:13 +0800

    20250715

---
+++ Leetcode/Medium/3201_Find the Maximum Length of Valid Subsequence I.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    internal class _3201_Find_the_Maximum_Length_of_Valid_Subsequence_I
+    {
+        public int MaximumLength(int[] nums)
+        {
+            var result = 0;
+
+            for (var i = 0; i < 2; i++)
+            {
+                var dp = new int[2];
+                foreach (var num in nums)
+                {
+                    var mod = num % 2;
+                    var pre = (i - mod + 2) % 2;
+                    dp[mod] = dp[pre] + 1;
+                    result = Math.Max(result, dp[mod]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Leetcode/Medium/3202_Find the Maximum Length of Valid Subsequence II.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Medium
+{
+    internal class _3202_Find_the_Maximum_Length_of_Valid_Subsequence_II
+    {
+        public int MaximumLength(int[] nums, int k)
+        {
+            var result = 0;
+
+            for (var i = 0; i < k; i++)
+            {
+                var dp = new int[k];
+                foreach (var num in nums)
+                {
+                    var mod = num % k;
+                    var pre = (i - mod + k) % k;
+                    dp[mod] = dp[pre] + 1;
+                    result = Math.Max(result, dp[mod]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
